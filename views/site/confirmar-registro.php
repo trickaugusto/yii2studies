18dff0da4f5a8a043c3e03ab37b3d407e446d193
trickzhavia <patrick@zhavia.com.br>
2023-04-04 10:06:58 -0300

    deleting some archives

--- views/site/confirmar-registro.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<p>Você enviou as seguintes informações:</p>
-
-<ul>
-    <li><label>Nome</label>: <?= Html::encode($model->name) ?></li>
-    <li><label>E-mail</label>: <?= Html::encode($model->email) ?></li>
-</ul>
